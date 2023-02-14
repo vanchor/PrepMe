dde237c1f5cdb429ee15d7a7b65707d6903d01b2
vanchor <vovanyakorev2@gmail.com>
2023-02-14 20:18:23 +0100

    init commit

---
+++ PerpMe.DAL/Implementations/BaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using PrepMe.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrepMe.DAL.Implementations
+{
+    internal class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+    }
+}
---
+++ PerpMe.DAL/Interfaces/IBaseRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrepMe.DAL.Interfaces
+{
+    public interface IBaseRepository<T>
+        where T : class
+    {
+        IEnumerable<T> Get();
+        T GetById(int id);
+        void Add(T item);
+        void AddRange(IEnumerable<T> items);
+
+        T Update(T item);
+        void Remove(T item);
+        void RemoveRange(IEnumerable<T> entities);
+
+        void SaveChanges();
+        Task SaveChangesAsync();
+        Task<IEnumerable<T>> GetAsync();
+    }
+}
