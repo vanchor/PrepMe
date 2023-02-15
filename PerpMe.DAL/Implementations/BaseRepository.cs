ca06eeea7e6f8d9b98796022b2940b34c6fec338
vanchor <vovanyakorev2@gmail.com>
2023-02-15 12:02:48 +0100

    added BaseRepository

--- PerpMe.DAL/Implementations/BaseRepository.cs
+++ PerpMe.DAL/Implementations/BaseRepository.cs
@@ -9,5 +9,61 @@ namespace PrepMe.DAL.Implementations
 {
     internal class BaseRepository<T> : IBaseRepository<T> where T : class
     {
+        // CONTEXT
+        public BaseRepository(/* Context */)
+        {
+                
+        }
+
+        public void Add(T item)
+        {
+            // return _context.Set<T>().Tolist();
+            throw new NotImplementedException();
+        }
+
+        public void AddRange(IEnumerable<T> items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Update(T item)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
