c7d0d08516d60c6e059c66c16c3abfc243cec6dd
ExoKamiz <66080830+ExoKamiz@users.noreply.github.com>
2023-02-19 23:47:56 +0100

    Update IProductService.cs

--- PrepMe.Services/Intefaces/IProductService.cs
+++ PrepMe.Services/Intefaces/IProductService.cs
@@ -6,6 +6,6 @@ namespace PrepMe.Services.Intefaces
     public interface IProductService
     {
         public Task<BaseResponse<List<Product>>> AddToDbAsync(IEnumerable<string> products);
-        public BaseResponse<List<Product>> FindByName(string name, int number = 10);
+        public BaseResponse<IEnumerable<Product>> FindByName(string name, int number = 10);
     }
 }
